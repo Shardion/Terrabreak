ae3bc8a0943b4b3facb4d45da42ba3aaf5eb7773
Shardion <22127191+Shardion@users.noreply.github.com>
2024-07-18 11:47:10 -0700

    Remove sudo (unnecessary)

--- Shardion.Terrabreak/Features/Sudo/SudoExpiryInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace Shardion.Terrabreak.Features.Sudo
-{
-    public class SudoExpiryInfo
-    {
-        public required string GuildId { get; set; }
-        public required string UserId { get; set; }
-    }
-}
